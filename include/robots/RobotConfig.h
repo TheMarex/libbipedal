3adef69a7b859c9ae9e66daa32f7d00671686641
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2014-07-21 17:31:42 +0200

    Add armar4 config

---
+++ include/robots/RobotConfig.h
@@ -0,0 +1,23 @@
+#ifndef __ROBOT_CONFIG_H__
+#define __ROBOT_CONFIG_H__
+
+namespace Bipedal
+{
+struct RobotConfig
+{
+	const char* robotName;
+	const char* WAIST_NODE_NAME;
+	const char* COL_NODESET_NAME;
+	const char* ROBOT_NODESET_NAME;
+	const char* LEGS_NODESET_NAME;
+	const char* CHEST_NODE_NAME;
+	const char* LEGS_IK_CHAIN;
+	const char* LEFT_LEG_TCP_NODE_NAME;
+	const char* RIGHT_LEG_TCP_NODE_NAME;
+	const char* LEFT_ANKLE_NODE_NAME;
+	const char* RIGHT_ANKLE_NODE_NAME;
+};
+}
+
+#endif
+
